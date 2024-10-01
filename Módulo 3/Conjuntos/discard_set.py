e404e902f18c641289179a895ca07f7a56fa03e1
Stephany <lifeuniversitary@gmail.com>
2024-10-01 11:21:42 -0300

    Métodos discard, pop, remove, len e in

---
+++ Módulo 3/Conjuntos/discard_set.py
@@ -0,0 +1,6 @@
+numeros = {1,2,3,4,5,6,7,8,9,0}
+
+print(numeros)
+numeros.discard(1)
+numeros.discard(50)
+print(numeros)
---
+++ Módulo 3/Conjuntos/remove_set.py
@@ -0,0 +1,5 @@
+numeros = {1,2,3,4,5,6,7,8,9,0}
+
+print(numeros)
+numeros.remove(9)
+print(numeros)
