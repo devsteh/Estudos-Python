c90aef35b09372933e5104d78b45e72fca287987
Stephany <lifeuniversitary@gmail.com>
2024-10-10 11:47:16 -0300

    Objetos date, datetime e time

---
+++ Módulo 3/Date Time/1_datetime.py
@@ -0,0 +1,15 @@
+from datetime import date, datetime, time
+#Forma mais prática da importação
+
+data = (date(2024, 10, 5))
+print(data)
+print(date.today()) #Data atual
+
+data_hora = datetime (2024, 10, 10, 11, 40, 00) 
+#Não passando o parâmetro de hora, virá zerado no print
+print(data_hora)
+print(datetime.today())
+
+#Retorna hora, passando parâmetros
+hora = time (11, 40, 0)
+print(hora)
